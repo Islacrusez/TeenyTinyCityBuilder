5c6471d373d08a6d9de4f52a1dbf584f886f8199
Islacrusez || m4rek <m4rek@hotmail.com>
2021-11-18 19:49:57 +0000

    Added rudimentary display of resource amount
    +Groundwork for button implementation

--- app/main.rb
+++ app/main.rb
@@ -1,6 +1,7 @@
 def tick(args)
 	init(args) unless args.state.ready == true
 	game_step(args)
+	update_display(args)
 end
 
 def init(args)
@@ -11,9 +12,15 @@ def init(args)
 	args.state.buildings = {}
 	args.state.buildings[:mine] = 0
 	
+	prepare_ui(args)
 	
 	args.state.ready = true
 	$gtk.notify!("Init complete!")
+end
+
+def prepare_ui(args)
+	args.state.ui[:ore] = [300, 300, "Ore: #{args.state.inventory[:ore]}"]
+	args.outputs.static_labels << args.state.ui[:ore]
 end
 
 def game_step(args)
@@ -28,4 +35,13 @@ def build_mine(args)
 	args.state.inventory[:ore] -= 20
 	args.state.buildings[:mine] += 1
 	args.state.production[:ore] += 5	
+end
+
+def update_display(args)
+	args.state.ui[:ore][2] = "Ore: #{args.state.inventory[:ore]}"
+end
+
+def make_button(x, y, w, h, text, function, target, args=$gtk.args)
+	
+	
 end

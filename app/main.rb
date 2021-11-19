344e27ddaf82e9b69c7e8346fe20899b0c586911
Islacrusez || m4rek <m4rek@hotmail.com>
2021-11-19 22:19:51 +0000

    Updated make_button
    Updated to create a general-case button visual. General-case click functionality still required.

--- app/main.rb
+++ app/main.rb
@@ -15,6 +15,8 @@ def init(args)
 	
 	prepare_ui(args)
 	
+	args.state.buttons[:mine] = make_button(600, 300, 80, 40, "Build", nil, :mine_button, args)
+	
 	args.state.ready = true
 	$gtk.notify!("Init complete!")
 end
@@ -43,8 +45,18 @@ def update_display(args)
 end
 
 def make_button(x, y, w, h, text, function, target, args=$gtk.args)
-	
-	
+	text_w, text_h = $gtk.calcstringbox(text)
+	args.render_target(target).height = h
+	args.render_target(target).width = w
+	out_x = x
+	out_y = y
+	x = 0
+	y = 0
+	args.render_target(target).borders << [x, y, w, h]
+	args.render_target(target).borders << [x, y+1, w-1, h-1]
+	args.render_target(target).borders << [x+2, y+2, w-4, h-4]
+	args.render_target(target).labels << [x + (w - text_w) / 2, y + (h + text_h) / 2 - 1, text]
+	[out_x, out_y, w, h, target]
 end
 
 def pretty_button(args)
@@ -52,18 +64,20 @@ def pretty_button(args)
 	y = 300
 	h = 40
 	w = 80
-	text = "Build"
+	# text = "Build"
 
-	text_w, text_h = $gtk.calcstringbox(text)
+	# text_w, text_h = $gtk.calcstringbox(text)
 
-	out = args.outputs
+	# out = args.outputs
 	
-	out.borders << [x, y, w, h]
-	out.borders << [x, y+1, w-1, h-1]
-	out.borders << [x+2, y+2, w-4, h-4]
-	out.labels << [x + (w - text_w) / 2, y + (h + text_h) / 2 - 1, text]
+	# out.borders << [x, y, w, h]
+	# out.borders << [x, y+1, w-1, h-1]
+	# out.borders << [x+2, y+2, w-4, h-4]
+	# out.labels << [x + (w - text_w) / 2, y + (h + text_h) / 2 - 1, text]
 	
 	check_mouse(x, y, w, h, args.inputs.mouse, args) if args.inputs.mouse.click
+	
+	args.outputs.sprites << args.state.buttons[:mine]
 
 end
 
